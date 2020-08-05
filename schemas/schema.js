f3d1258fe56a54e4d8f4a9c40b3547c1d74de2d7
Julien <julien.juret@gmail.com>
2020-08-06 00:25:26 +0200

    local db for test

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,1 +1,38 @@
 const graphQL = require("graphql");
+const axios = require("axios")
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema
+} = graphQL;
+
+const UserType = new GraphQLObjectType({
+    name: "User",
+    fields: {
+        id: { type: GraphQLString },
+        firstName: { type: GraphQLString },
+        email: { type: GraphQLString },
+        password: { type: GraphQLString },
+        role: { type: GraphQLInt }
+    }
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLString } },
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/users/${args.id}`).then(response => {
+                    return response.data;
+                })
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const userSchema = require("./schemas/schema");
 const server = express();
 
 server.use("/graphql", graphqlHTTP({
-    graphiql: true
+    graphiql: true,
+    schema: userSchema
 }))
+
 server.listen(4000, () => {
     console.log("Serveur en écoute sur le port 4000");
 })
